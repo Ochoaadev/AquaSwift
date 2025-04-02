8f1a1102e4d1d5733f5595ad57d9538fc6009dcc
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2025-04-01 20:29:16 -0400

    Agregando Filtro de Mis Competencias

--- FrontEnd/src/views/User/MisCompetencias.jsx
+++ FrontEnd/src/views/User/MisCompetencias.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../contexts/AuthProvider";
 import { api } from "../../service/apiService";
 import { useNavigate } from "react-router-dom";
-import CompetenciasFilter from '../../components/competenciasFilter';
 import trofeo from "/trofeo.png";
 
 const MisCompetencias = () => {
@@ -12,6 +11,7 @@ const MisCompetencias = () => {
   const [error, setError] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
   const [pruebasSeleccionadas, setPruebasSeleccionadas] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const navigate = useNavigate();
 
@@ -25,13 +25,13 @@ const MisCompetencias = () => {
         }
 
         const token = localStorage.getItem("token");
-        
+
         // Obtener las inscripciones del atleta
         const response = await api.inscripcion.getByAtleta(userId, token);
-        
+
         // Procesar la respuesta
         let inscripcionesData = [];
-        
+
         if (Array.isArray(response)) {
           inscripcionesData = response;
         } else if (response?.data) {
@@ -42,17 +42,18 @@ const MisCompetencias = () => {
 
         // Filtrar inscripciones válidas con competencia y prueba
         const inscripcionesValidas = inscripcionesData.filter(
-          insc => insc && 
-                 insc._id && 
-                 insc.Competencia && 
-                 insc.Prueba &&
-                 insc.Competencia.Imagen
+          insc =>
+            insc &&
+            insc._id &&
+            insc.Competencia &&
+            insc.Prueba &&
+            insc.Competencia.Imagen
         );
 
         // Agrupar inscripciones por competencia
         const agrupadas = inscripcionesValidas.reduce((acc, inscripcion) => {
           const competenciaId = inscripcion.Competencia._id;
-          
+
           if (!acc[competenciaId]) {
             acc[competenciaId] = {
               competencia: inscripcion.Competencia,
@@ -61,13 +62,13 @@ const MisCompetencias = () => {
               estado: inscripcion.Estado
             };
           }
-          
+
           acc[competenciaId].pruebas.push(inscripcion.Prueba);
           // Mantener la fecha de inscripción más reciente
           if (new Date(inscripcion.FechaInscripcion) > new Date(acc[competenciaId].fechaInscripcion)) {
             acc[competenciaId].fechaInscripcion = inscripcion.FechaInscripcion;
           }
-          
+
           return acc;
         }, {});
 
@@ -87,17 +88,16 @@ const MisCompetencias = () => {
     setPruebasSeleccionadas(pruebas);
     setModalOpen(true);
   };
-  
+
   const closeModal = () => {
     setModalOpen(false);
     setPruebasSeleccionadas([]);
   };
-  
 
   const getDisciplinaStyles = (disciplina) => {
     const colorClasses = ["text-primary-100", "text-primary-300", "text-primary-400"];
     const bgColorClasses = ["bg-primary-150", "bg-primary-300", "bg-primary-450"];
-  
+
     // Determina el índice basado en la disciplina
     let index;
     switch (disciplina.toLowerCase()) {
@@ -114,30 +114,26 @@ const MisCompetencias = () => {
         index = 3;
         break;
     }
-  
+
     // Devuelve un objeto con las clases de texto y fondo
     return {
       textClass: colorClasses[index] || "text-primary-100",   // Clase para el texto
       bgClass: bgColorClasses[index] || "bg-primary-150"      // Clase para el fondo
     };
   };
-  
-  
 
   const organizarCompetenciasPorDisciplina = (inscripciones) => {
     return inscripciones.reduce((acc, grupo) => {
       const disciplina = grupo.competencia.Disciplina || "Otros";
-  
+
       if (!acc[disciplina]) {
         acc[disciplina] = [];
       }
-  
+
       acc[disciplina].push(grupo);
       return acc;
     }, {});
   };
-  
-  
 
   if (loading) return (
     <div className="flex justify-center items-center h-64">
@@ -151,6 +147,19 @@ const MisCompetencias = () => {
     </div>
   );
 
+  // Filtrar las competencias según el término de búsqueda
+  const filteredInscripciones = Object.entries(inscripcionesAgrupadas).reduce((acc, [disciplina, grupos]) => {
+    const filteredGrupos = grupos.filter(grupo =>
+      grupo.competencia.Nombre.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    
+    if (filteredGrupos.length > 0) {
+      acc[disciplina] = filteredGrupos;
+    }
+
+    return acc;
+  }, {});
+
   return (
     <div className="container mx-auto py-8 w-5/6">
       <div className="p-6 md:w-11/12 lg:w-5/6 mx-auto poppins">
@@ -158,112 +167,114 @@ const MisCompetencias = () => {
           <img src={trofeo} alt="trofeo" className="lg:w-1/12 md:w-2/12 w-3/12 mr-1" />
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold lg:mt-12 md:mt-12 mt-3 text-start">Mis Competencias</h1>
         </div>
-        <CompetenciasFilter />
-      </div>
-  
-      {inscripcionesAgrupadas.length === 0 ? (
-        <div className="bg-blue-50 border-l-4 border-blue-500 text-blue-700 p-4 rounded">
-          <p>No tienes inscripciones activas en este momento.</p>
+
+        {/* Barra de búsqueda */}
+        <div>
+          <div className="flex justify-center mt-6 text-black">
+            <input
+              type="text"
+              placeholder="Buscar Competencia..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="px-4 py-2 border border-gray-300 rounded-lg w-full md:w-4/6 lg:3/6"
+            />
+          </div>
         </div>
-      ) : Object.entries(inscripcionesAgrupadas).map(([disciplina, grupos]) => {
-        // Obtener las clases para fondo y texto de la disciplina
-        const { bgClass, textClass } = getDisciplinaStyles(disciplina);
-  
-        return (
-          <div key={disciplina}>
-            <h1 className={`text-4xl mt-5 font-bold ${textClass}`}>{disciplina}</h1>
-            <div className="w-8/12 md:w-5/12 lg:w-3/12 h-1 bg-primary-0 mb-5"></div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
-              {grupos.map((grupo, index) => {
-                // Usar las clases bgClass para el fondo
-                return (
-                  <div
-                    key={index}
-                    className={`rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow ${bgClass}`}
-                  >
-                    <div className="grid grid-cols-2">
-                      {/* Sección de imagen */}
-                      <div className="p-4 flex justify-center items-center">
-                        <img
-                          src={grupo.competencia.Imagen.url || "/default-competition.jpg"}
-                          alt={grupo.competencia.Nombre}
-                          className="h-48 w-72 rounded-lg mx-auto lg:mx-0 sm:w-64 md:w-72 lg:w-90 lg:h-56"
-                          onError={(e) => {
-                            e.target.src = "/default-competition.jpg";
-                          }}
-                        />
-                      </div>
-  
-                      {/* Sección de información */}
-                      <div className="p-3 mt-3 w-full">
-                        {/* Encabezado con nombre de competencia */}
-                        <div className="flex justify-between items-start mb-2">
+      </div>
+
+      {searchTerm && Object.keys(filteredInscripciones).length === 0 ? (
+        <h1 className="text-center text-3xl font-bold text-red-500 mt-12 mb-12">No se encontraron competencias</h1>
+      ) : (
+        Object.entries(filteredInscripciones).map(([disciplina, grupos]) => {
+          const { bgClass, textClass } = getDisciplinaStyles(disciplina);
+
+          return (
+            <div key={disciplina}>
+              <h1 className={`text-4xl mt-5 font-bold ${textClass}`}>{disciplina}</h1>
+              <div className="w-8/12 md:w-5/12 lg:w-3/12 h-1 bg-primary-0 mb-5"></div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
+                {grupos.map((grupo, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className={`rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow ${bgClass}`}
+                    >
+                      <div className="grid grid-cols-1 lg:grid-cols-2">
+                        <div className="p-4 flex justify-center items-center">
+                          <img
+                            src={grupo.competencia.Imagen.url || "/default-competition.jpg"}
+                            alt={grupo.competencia.Nombre}
+                            className="h-48 w-72 rounded-lg mx-auto lg:mx-0 sm:w-64 md:w-72 lg:w-90 lg:h-56"
+                            onError={(e) => {
+                              e.target.src = "/default-competition.jpg";
+                            }}
+                          />
+                        </div>
+
+                        <div className="p-3 mt-3 w-full text-center lg:text-left">
                           <h2 className="text-xl font-bold text-gray-800">
                             {grupo.competencia.Nombre || "Competencia no especificada"}
                           </h2>
-                        </div>
-  
-                        {/* Detalles de la inscripción */}
-                        <div className="grid gap-4 mb-1 text-lg">
-                          <div>
-                            <p className=" text-gray-100 font-bold">Fecha de inscripción:</p>
-                            <p className="text-gray-800 -mt-1">
-                              {grupo.fechaInscripcion
-                                ? new Date(grupo.fechaInscripcion).toLocaleDateString('es-ES', {
-                                    day: '2-digit',
-                                    month: 'long',
-                                    year: 'numeric'
-                                  })
-                                : "Fecha no disponible"}
-                            </p>
+                          <div className="grid gap-4 mb-1 text-lg">
                             <div>
-                              <p className=" text-gray-100 mt-2 font-bold">Fecha competencia:</p>
+                              <p className=" text-gray-100 font-bold">Fecha de inscripción:</p>
                               <p className="text-gray-800 -mt-1">
-                                {grupo.competencia.Fecha
-                                  ? new Date(grupo.competencia.Fecha).toLocaleDateString('es-ES', {
+                                {grupo.fechaInscripcion
+                                  ? new Date(grupo.fechaInscripcion).toLocaleDateString('es-ES', {
                                       day: '2-digit',
                                       month: 'long',
                                       year: 'numeric'
                                     })
-                                  : "Por definir"}
+                                  : "Fecha no disponible"}
                               </p>
+                              <div>
+                                <p className=" text-gray-100 mt-2 font-bold">Fecha competencia:</p>
+                                <p className="text-gray-800 -mt-1">
+                                  {grupo.competencia.Fecha
+                                    ? new Date(grupo.competencia.Fecha).toLocaleDateString('es-ES', {
+                                        day: '2-digit',
+                                        month: 'long',
+                                        year: 'numeric'
+                                      })
+                                    : "Por definir"}
+                                </p>
+                              </div>
                             </div>
                           </div>
-                        </div>
-  
-                        <div className="grid grid-cols-1 mx-auto">
-                          <div className="mb-1">
-                            <button 
-                              onClick={() => openModal(grupo.pruebas)}
-                              className="w-full bg-gradient-to-r from-[#1E40AF] to-[#9333EA] text-white font-bold py-1 px-3 rounded-lg transition hover:scale-105 duration-200"
-                            >
-                              Ver Pruebas
-                            </button>
-                          </div>
-                          <div>
-                            <button 
-                              className="w-full bg-gradient-to-r from-[#1E40AF] to-[#9333EA] text-white font-bold py-1 px-3 rounded-lg transition hover:scale-105 duration-200"
-                              disabled
-                              title="Funcionalidad próximamente disponible"
-                            >
-                              Mostrar resultados
-                            </button>
+                          <div className="grid grid-cols-1 mx-auto">
+                            <div className="mb-1">
+                              <button
+                                onClick={() => openModal(grupo.pruebas)}
+                                className="w-full bg-gradient-to-r from-[#1E40AF] to-[#9333EA] text-white font-bold py-1 px-3 rounded-lg transition hover:scale-105 duration-200"
+                              >
+                                Ver Pruebas
+                              </button>
+                            </div>
+                            <div>
+                              <button
+                                className="w-full bg-gradient-to-r from-[#1E40AF] to-[#9333EA] text-white font-bold py-1 px-3 rounded-lg transition hover:scale-105 duration-200"
+                                disabled
+                                title="Funcionalidad próximamente disponible"
+                              >
+                                Mostrar resultados
+                              </button>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        );
-      })}
-  
+          );
+        })
+      )}
+
       {/* Modal para mostrar las pruebas */}
       {modalOpen && (
         <div className="text-black fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-5/6 lg:w-1/3">
             <h3 className="text-lg font-semibold text-center mb-2">Pruebas Inscritas</h3>
             <ul className="grid grid-cols-2 gap-1">
               {pruebasSeleccionadas.map((prueba, i) => (
@@ -275,11 +286,9 @@ const MisCompetencias = () => {
                 </li>
               ))}
             </ul>
-  
-            {/* Contenedor para centrar el botón */}
             <div className="w-full flex justify-center mt-2">
-              <button 
-                onClick={closeModal} 
+              <button
+                onClick={closeModal}
                 className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
               >
                 Cerrar
@@ -290,7 +299,6 @@ const MisCompetencias = () => {
       )}
     </div>
   );
-  
 };
 
 export default MisCompetencias;
